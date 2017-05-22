f22b3bb2afd21a9113fe442661bf76b40a5e5a1f
NadimRai <rai_nadim@yahoo.com>
2017-05-22 15:06:54 -0700

    stripe form added to home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
+  resources :charges
   
 
   get 'about', to: 'pages#about'
