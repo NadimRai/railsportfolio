300633b3dc241e36ed353a0bc926554042d5e951
NadimRai <rai_nadim@yahoo.com>
2017-05-22 23:08:53 -0700

    generated Product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170523054218) do
+ActiveRecord::Schema.define(version: 20170523060323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,19 @@ ActiveRecord::Schema.define(version: 20170523054218) do
     t.text     "thumb_image"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title"
+    t.string   "subtitle"
+    t.string   "image_name"
+    t.string   "sku"
+    t.string   "download_url"
+    t.string   "details"
+    t.text     "description"
+    t.decimal  "price"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "purchases", force: :cascade do |t|
